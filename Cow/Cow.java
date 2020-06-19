b48495651416c4226597d8f365316ca89db09a8a
Darnesha Johnson <66345516+DarneshaJ@users.noreply.github.com>
2020-06-18 20:16:22 -0700

    Add files via upload

---
+++ Cow/Cow.java
@@ -0,0 +1,17 @@
+public class Cow {
+    //attributes: name 
+    public String name;
+
+//methods
+
+public Cow(String name){
+    this.name=name;
+}
+
+public void printCow(){
+
+    System.out.println("Name: " + this.name);
+}
+
+
+}
